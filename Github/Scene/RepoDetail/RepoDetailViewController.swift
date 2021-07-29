c839daea3fec4728b5dc93f2033950111b1966a8
bekir tura <bekir.tura@ykteknoloji.com.tr>
2021-07-29 20:56:38 +0300

    development project detail pages

--- Github/Scene/RepoDetail/RepoDetailViewController.swift
+++ Github/Scene/RepoDetail/RepoDetailViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class RepoDetailViewController: BaseViewController {
 
+    //MARK:  START -- IBOutlets
+    @IBOutlet private weak var lblRepoName: UILabel!
+    @IBOutlet private weak var lblOwnerName: UILabel!
+    @IBOutlet private weak var imgAvatar: UIImageView!
+    @IBOutlet weak var lblWatchedCount: UILabel!
+    @IBOutlet weak var lblForkCount: UILabel!
+    @IBOutlet weak var lblProgrammingLanguage: UILabel!
+    //MARK:  END -- IBOutlets
     var presenter: RepoDetailViewPresenterProtocol?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        presenter?.getRepo()
 
         // Do any additional setup after loading the view.
     }
@@ -32,6 +41,12 @@ class RepoDetailViewController: BaseViewController {
 
 extension RepoDetailViewController: RepoDetailViewProtocol{
     func repoLoadUI(repo: RepoPresentation?) {
-        self.navigationController?.title = repo?.repoName
+        self.title = repo?.repoName
+        self.lblRepoName.text = repo?.repoName
+        self.lblOwnerName.text = repo?.ownerName
+        self.imgAvatar.setImage(fromString: repo?.imageUrl)
+        self.lblForkCount.text = "\(String(describing: repo?.forksCount ?? 0))"
+        self.lblWatchedCount.text = "\(String(describing: repo?.watchersCount ?? 0))"
+        self.lblProgrammingLanguage.text = repo?.programmingLanguage
     }
 }
